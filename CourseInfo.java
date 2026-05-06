77981581bd868f74b5fc96226ece67085444b4b8
William Wian <williamwian@go.rmc.edu>
2026-05-06 13:44:52 -0400

    Initial version of CourseInfo

---
+++ CourseInfo.java
@@ -0,0 +1,11 @@
+// CourseInfo.java
+
+public class CourseInfo{
+	public static void main(String[] args){
+		System.out.print("Course Name: Object-Oriented Programming\n");
+		System.out.print("Instructor: Maha Elouni\n");
+		System.out.print("Semester: Spring 2026\n");
+		System.out.print("Credits: 4\n");
+		System.out.print("Classroom: Copley 204\n");
+	}
+}
